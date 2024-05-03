baafe02b4f924b838dd263eb89c22519e51812e3
ldkjvvier <javier.madariaga02@inacapmail.cl>
2024-05-03 04:22:13 -0400

    chore: Update color in index.css and fix routes in routes.tsx

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,7 +1,7 @@
-import { createHashRouter } from "react-router-dom"
-import { HomePage } from "./pages/HomePage"
-import { PokemonPage } from "./pages/PokemonPage"
+import { createHashRouter } from 'react-router-dom'
+import { HomePage } from './pages/HomePage'
+import { NotFound } from './components/NotFound'
 export const router = createHashRouter([
 	{ path: '/', element: <HomePage /> },
-	{ path: '/:id', element: <PokemonPage /> },
+	{ path: '*', element: <NotFound/>}
 ])
