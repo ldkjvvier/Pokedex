ba7c348a3fa3d42eb7f5035205d1ef970f88bebe
ldkjvvier <javier.madariaga02@inacapmail.cl>
2024-05-03 02:09:44 -0400

    chore: Update routes for Pokedex pages

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -5,8 +5,8 @@ import { PokemonPage } from './pages/PokemonPage'
 export const AppRouter = (): JSX.Element => {
 	return (
 		<Routes>
-			<Route path="/pokedex" element={<HomePage />} />
-			<Route path="pokedex/:id" element={<PokemonPage />} />
+			<Route path="/Pokedex" element={<HomePage />} />
+			<Route path="Pokedex/:id" element={<PokemonPage />} />
 			<Route path="*" element={<HomePage />} />
 		</Routes>
 	)
