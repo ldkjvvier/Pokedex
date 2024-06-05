e28e6b525bdd6fc39159d31b7bd9f3e3f05ea5d7
ldkjvvier <javier.madariaga02@inacapmail.cl>
2024-06-05 02:06:05 -0400

    feat: Add Pokemon service for retrieving Pokemon and types

---
+++ src/modules/Pokemon/services/pokemonService.ts
@@ -0,0 +1,12 @@
+import { Pokedex } from 'pokeapi-js-wrapper'
+const P = new Pokedex()
+
+export const getPokemon = async (id: number) => {
+  const Pokemon = await P.getPokemonByName(id)
+  return Pokemon
+}
+
+export const getType = async (id: number) => {
+	const Pokemon = await P.getTypeByName(id)
+	return Pokemon
+}
