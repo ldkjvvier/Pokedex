0d50c552a7f4f30b8942fec71cee844009fb133d
ldkjvvier <javier.madariaga02@inacapmail.cl>
2024-06-05 22:07:03 -0400

    refactor: Update PokemonEvolutions component styles and structure

--- src/modules/Pokemon/components/evolutions/PokemonEvolutions.tsx
+++ src/modules/Pokemon/components/evolutions/PokemonEvolutions.tsx
@@ -1,17 +1,10 @@
 import { PokemonEvolutionProfile } from './PokemonEvolutionsProfile'
 export const PokemonEvolutions = (): JSX.Element => {
-
 	return (
 		<div className="flex justify-center">
-			<div
-				className="bg-background-image w-11/12 opacity-90 text-white text-start p-6 mb-48"
-				style={{
-					backgroundColor: '#424242',
-					borderRadius: '5px 5px 5px 50px',
-				}}
-			>
+			<div className="bg-background-image bg-[#424242] w-11/12 opacity-90 text-white text-start p-6 mb-24 rounded-[5px] rounded-bl-[50px] shadow-lg ">
 				<h4 className="font-bold text-2xl mb-6">Evoluciones</h4>
-				<div className="grid grid-cols-3 h-full">
+				<div className="grid md:grid-cols-3 gap-12">
 					<div className="first">
 						<PokemonEvolutionProfile />
 					</div>
