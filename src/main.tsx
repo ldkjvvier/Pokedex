e0a46e6dc39a17eda3f1043a8629158a430e1cca
ldkjvvier <javier.madariaga02@inacapmail.cl>
2024-06-10 18:41:24 -0400

    refactor: Update main.tsx to use react-query for API data fetching

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes.tsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 const root = ReactDOM.createRoot(document.getElementById('root')!)
-
+const queryClient = new QueryClient()
 root.render(
 	<React.StrictMode>
-			<RouterProvider router={router}/>
+		<QueryClientProvider client={queryClient}>
+			<RouterProvider router={router} />
+		</QueryClientProvider>
 	</React.StrictMode>
 )
