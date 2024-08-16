bf2cab5e1ca2a5f9a9d8200543aa0866a85bd504
Javier Madariaga <145422138+ldkjvvier@users.noreply.github.com>
2024-08-16 19:45:36 -0400

    Test: debug

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,10 @@
 import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { AppRoutes } from './routes.tsx'
-import {
-	QueryClient,
-	QueryClientProvider,
-} from '@tanstack/react-query'
+
 const root = ReactDOM.createRoot(document.getElementById('root')!)
-const queryClient = new QueryClient()
 root.render(
 	<React.StrictMode>
-		<QueryClientProvider client={queryClient}>
-			<AppRoutes />
-		</QueryClientProvider>
+		<p>HOLA MUNDO</p>
 	</React.StrictMode>
 )
