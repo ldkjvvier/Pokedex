5cc95b831a9be3aa7d84618094d81436ec12b087
Javier Madariaga <145422138+ldkjvvier@users.noreply.github.com>
2024-08-16 19:38:38 -0400

    chore: implement new method for routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,17 @@
 import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
-import { router } from './routes.tsx'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { AppRoutes } from './routes.tsx'
+import {
+	QueryClient,
+	QueryClientProvider,
+} from '@tanstack/react-query'
 const root = ReactDOM.createRoot(document.getElementById('root')!)
 const queryClient = new QueryClient()
 root.render(
 	<React.StrictMode>
 		<QueryClientProvider client={queryClient}>
-			<RouterProvider router={router} />
+			<AppRoutes />
 		</QueryClientProvider>
 	</React.StrictMode>
 )
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,16 @@
-import { createHashRouter } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { HomePage } from './modules/Home'
 import { NotFound } from './components/NotFound'
 import { PokemonPage } from './modules/Pokemon/pages/PokemonPage'
 
-export const router = createHashRouter([
-	{ path: '/', element: <HomePage /> },
-	{ path: '/pokemon/:id', element: <PokemonPage /> },
-	{ path: '*', element: <NotFound /> },
-])
+export const AppRoutes = () => {
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<HomePage />} />
+				<Route path="/pokemon/:id" element={<PokemonPage />} />
+				<Route path="*" element={<NotFound />} />
+			</Routes>
+		</BrowserRouter>
+	)
+}
