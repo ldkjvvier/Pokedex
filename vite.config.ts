898209cb861e579bf905555ec6d7705ccd4eb6c2
Javier Madariaga <145422138+ldkjvvier@users.noreply.github.com>
2024-08-16 18:24:48 -0400

    Fix: background not displayed

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: 'https://ldkjvvier.github.io/Pokedex/',
 	resolve: {
 		alias: [
 			{ find: '@', replacement: path.resolve(__dirname, 'src') },
