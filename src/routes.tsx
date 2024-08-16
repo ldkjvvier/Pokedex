2f26f25e18a5463a551a32a47b6ec0aecb4db25c
Javier Madariaga <145422138+ldkjvvier@users.noreply.github.com>
2024-08-16 19:52:59 -0400

    chore: updated to latest version react-router-dom

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,7 +1,16 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HomePage } from './modules/Home'
+import { NotFound } from './components/NotFound'
+import { PokemonPage } from './modules/Pokemon/pages/PokemonPage'
+
 export const AppRoutes = () => {
 	return (
-		<div>
-			<h1>Test Page</h1>
-		</div>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<HomePage />} />
+				<Route path="/pokemon/:id" element={<PokemonPage />} />
+				<Route path="*" element={<NotFound />} />
+			</Routes>
+		</BrowserRouter>
 	)
 }
