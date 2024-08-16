ba41ed9a08b82d8cfc0c8db888ab18817186c862
Javier Madariaga <145422138+ldkjvvier@users.noreply.github.com>
2024-08-16 19:49:50 -0400

    test: app routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
+import { AppRoutes } from './routes.tsx'
+import {
+	QueryClient,
+	QueryClientProvider,
+} from '@tanstack/react-query'
 const root = ReactDOM.createRoot(document.getElementById('root')!)
+const queryClient = new QueryClient()
 root.render(
 	<React.StrictMode>
-		<p>HOLA MUNDO</p>
+		<QueryClientProvider client={queryClient}>
+			<AppRoutes />
+		</QueryClientProvider>
 	</React.StrictMode>
 )
