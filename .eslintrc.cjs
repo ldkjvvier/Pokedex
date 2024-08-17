e467fe88bba192b97c199a5e1d60cdee438894d4
Javier Madariaga <145422138+ldkjvvier@users.noreply.github.com>
2024-08-16 20:21:37 -0400

    chore: implement changes in eslint and some fixes

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,17 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended'],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json'], // Asegúrate de que estos archivos existan
+    tsconfigRootDir: __dirname
   },
-}
+  rules: {
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }]
+  }
+};
--- src/modules/Home/pages/HomePage.tsx
+++ src/modules/Home/pages/HomePage.tsx
@@ -1,36 +1,31 @@
-import { PokemonCard } from '../components/PokemonCard'
-import React from 'react'
-import { usePokemons } from '../hooks/usePokemons'
-import { Loader } from '../../../components/Loader'
-import { Pokemon } from 'pokeapi-js-wrapper'
+import { PokemonCard } from '../components/PokemonCard';
+import React from 'react';
+import { usePokemons } from '../hooks/usePokemons';
+import { Loader } from '../../../components/Loader';
+import { Pokemon } from 'pokeapi-js-wrapper';
 export const HomePage = (): JSX.Element => {
-	const { isLoading, pokemons, error, fetchMorePokemons } =
-		usePokemons()
-	if (error) return <p>Error: {error.message}</p>
-	if (!pokemons || pokemons.length < 1) return <p>No Pokemons</p>
+  const { isLoading, pokemons, error, fetchMorePokemons } = usePokemons();
+  if (error) return <p>Error: {error.message}</p>;
+  if (isLoading) return <Loader />;
+  if (!pokemons || pokemons.length < 1) return <p>No Pokemons</p>;
 
-	return (
-		<>
-			<main>
-				<div className="bg-white px-16 py-16 max-w-3/4">
-					<div className="grid sm:grid-cols-4 gap-3">
-						{isLoading && <Loader />}
+  return (
+    <>
+      <main>
+        <div className="bg-white px-16 py-16 max-w-3/4">
+          <div className="grid sm:grid-cols-4 gap-3">
+            {pokemons.map((pokemon: Pokemon) => (
+              <React.Fragment key={`${pokemon.name}-${pokemon.id}`}>
+                <PokemonCard pokemon={pokemon} />
+              </React.Fragment>
+            ))}
+          </div>
 
-						{pokemons?.map((pokemon: Pokemon) => (
-							<React.Fragment key={`${pokemon.name}-${pokemon.id}`}>
-								<PokemonCard pokemon={pokemon} />
-							</React.Fragment>
-						))}
-					</div>
-
-					<button
-						className="bg-cyan-500 hover:bg-cyan-600 p-3 rounded mt-10"
-						onClick={() => fetchMorePokemons()}
-					>
-						Load More Pokemons
-					</button>
-				</div>
-			</main>
-		</>
-	)
-}
+          <button className="bg-cyan-500 hover:bg-cyan-600 p-3 rounded mt-10" onClick={() => fetchMorePokemons()}>
+            Load More Pokemons
+          </button>
+        </div>
+      </main>
+    </>
+  );
+};
