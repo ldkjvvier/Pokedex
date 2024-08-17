84c50edaf8071c030c45c2f7ed5062753610c7a5
Javier Madariaga <145422138+ldkjvvier@users.noreply.github.com>
2024-08-16 21:12:50 -0400

    test

--- src/modules/Home/components/PokemonCard.tsx
+++ src/modules/Home/components/PokemonCard.tsx
@@ -1,4 +1,3 @@
-import { Color } from '../interfaces/Color';
 import { Link } from 'react-router-dom';
 import { Pokemon } from 'pokeapi-js-wrapper';
 import { capitalizeFirstLetter } from '@/utils/capitalizeFirstLetter';
@@ -21,20 +20,7 @@ export const PokemonCard = ({ pokemon }: { pokemon: Pokemon }): JSX.Element => {
       <section className="p-3 text-start">
         <p className="flexoBold text-gray-500 text-xs font-bold">N.° {pokemon.id}</p>
         <h5 className="text-black text-lg pt-1.5 font-bold">{capitalizeFirstLetter(pokemon.name)}</h5>
-        <div>
-          {pokemon.types.map((type: { type: { name: string } }) => (
-            <span
-              className={`flexoMedium text-gray-500 rounded px-6 py-1 text-[11px] mr-2 mt-2`}
-              key={type.type.name}
-              style={{
-                background: Color[type.type.name as keyof typeof Color]?.background ?? 'default-background-color',
-                color: Color[type.type.name as keyof typeof Color]?.text ?? 'default-text-color'
-              }}
-            >
-              {capitalizeFirstLetter(type.type.name)}
-            </span>
-          ))}
-        </div>
+        <div></div>
       </section>
     </div>
   );
