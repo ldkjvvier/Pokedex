d7b03ea16b77bc143122bb6213b57f237b35177d
Javier Madariaga <145422138+ldkjvvier@users.noreply.github.com>
2024-08-16 20:33:29 -0400

    update: verify if is string

--- src/utils/capitalizeFirstLetter.ts
+++ src/utils/capitalizeFirstLetter.ts
@@ -1,3 +1,6 @@
 export const capitalizeFirstLetter = (string: string) => {
-	return string.charAt(0).toUpperCase() + string.slice(1)
-}
+  if (typeof string !== 'string' || string.length === 0) {
+    return '';
+  }
+  return string.charAt(0).toUpperCase() + string.slice(1);
+};
