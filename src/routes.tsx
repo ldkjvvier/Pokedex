2e860e0112c8cb6d2aefb957083acf0a886b2e4a
Javier Madariaga <145422138+ldkjvvier@users.noreply.github.com>
2024-10-04 00:33:06 -0300

    update: base url

--- src/routes.tsx
+++ src/routes.tsx
@@ -5,10 +5,10 @@ import { PokemonPage } from './modules/Pokemon/pages/PokemonPage';
 
 export const AppRoutes = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/pokedex">
       <Routes>
-        <Route path="/pokedex/" element={<HomePage />} />
-        <Route path="pokedex/:name" element={<PokemonPage />} />
+        <Route path="" element={<HomePage />} />
+        <Route path="/:name" element={<PokemonPage />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
