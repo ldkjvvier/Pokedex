f8c8a14e130d2d539751f63970822eed7a120190
Javier Madariaga <145422138+ldkjvvier@users.noreply.github.com>
2024-10-17 19:02:16 -0300

    feat: implement better animations and fix audit

--- src/modules/Home/pages/HomePage.tsx
+++ src/modules/Home/pages/HomePage.tsx
@@ -3,28 +3,29 @@ import React from 'react';
 import { usePokemons } from '../hooks/usePokemons';
 import { Loader } from '../../../components/Loader';
 import { Pokemon } from 'pokeapi-js-wrapper';
+import { useAutoAnimate } from '@formkit/auto-animate/react';
 export const HomePage = (): JSX.Element => {
   const { isLoading, pokemons, error, fetchMorePokemons } = usePokemons();
+  const [parent] = useAutoAnimate();
+
   if (error) return <p>Error: {error.message}</p>;
   if (isLoading || pokemons.length < 1) return <Loader />;
 
   return (
-    <>
-      <main>
-        <div className="bg-white px-16 py-16 max-w-3/4">
-          <div className="grid sm:grid-cols-4 gap-3">
-            {pokemons?.map((pokemon: Pokemon) => (
-              <React.Fragment key={`${pokemon.name}-${pokemon.id}`}>
-                <PokemonCard pokemon={pokemon} />
-              </React.Fragment>
-            )) || <p>No Pokemons</p>}
-          </div>
-
-          <button className="bg-cyan-500 hover:bg-cyan-600 p-3 rounded mt-10" onClick={() => void fetchMorePokemons()}>
-            Load More Pokemons
-          </button>
+    <main>
+      <div className="bg-white px-16 py-16 max-w-3/4">
+        <div className="grid sm:grid-cols-4 gap-3" ref={parent}>
+          {pokemons?.map((pokemon: Pokemon) => (
+            <React.Fragment key={`${pokemon.name}-${pokemon.id}`}>
+              <PokemonCard pokemon={pokemon} />
+            </React.Fragment>
+          )) || <p>No Pokemons</p>}
         </div>
-      </main>
-    </>
+
+        <button className="bg-cyan-500 hover:bg-cyan-600 p-3 rounded mt-10" onClick={() => void fetchMorePokemons()}>
+          Load More Pokemons
+        </button>
+      </div>
+    </main>
   );
 };
